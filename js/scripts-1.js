70aaf63ff6d639a38d115449342af501a3309177
PavelRoR <pavelvp2182@yandex.ru>
2021-04-08 17:34:09 +0500

    Thanks page is adapted

--- js/scripts-1.js
+++ js/scripts-1.js
@@ -47,8 +47,34 @@ $(document).ready(function() {
     // Модалка
     $('.button-up').magnificPopup({
         type: 'inline',
-        midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+        midClick: true, 
         mainClass: 'mfp-fade'
     });
+    // Мобильное меню
+    $(function(){
+        var mobMenu = $('.mobile-menu'),
+            headNav = $('.header-nav'),
+            mml1 = $('.mobile-menu-line-1'),
+            mml2 = $('.mobile-menu-line-2'),
+            mml3 = $('.mobile-menu-line-3'),
+            body = $('body');
+        mobMenu.on('click', function () {
+            if (!headNav.hasClass('nav-active')) {
+                $(this).css('position' , 'fixed');
+                body.css('overflow' , 'hidden');
+                headNav.addClass('nav-active');
+                mml1.addClass('switched');
+                mml2.addClass('switched');
+                mml3.addClass('switched');
+            } else {
+                $(this).css('position' , 'absolute');
+                body.css('overflow' , 'auto');
+                headNav.removeClass('nav-active');
+                mml1.removeClass('switched');
+                mml2.removeClass('switched');
+                mml3.removeClass('switched');
+            }
+        });
+        });
     /*Конец документа*/
     });
